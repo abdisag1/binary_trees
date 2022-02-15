58a46541555d51c8fe7a9476f48c25239cf441dd
abdisag1 <myabdisa@gmail.com>
2022-02-15 03:06:28 +0000

    done insertion to the left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
+binary_tree_t *new_bt, *temp;
+new_bt = malloc(sizeof(binary_tree_t));
+if (new_bt ==NULL)
+return NULL;
+new_bt->parent = parent ;
+new_bt->left = NULL;
+new_bt->right = NULL;
+new_bt->n = value;
+if (parent->left != NULL  ){
+temp = parent->left;
+parent->left= new_bt;
+new_bt->left = temp; }}
