198cf22553c46f87a718ed798f60dbd7e4ad5130
abdisag1 <myabdisa@gmail.com>
2022-02-18 17:33:33 +0000

    fixed betty styles

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 size_t binary_tree_leaves(const binary_tree_t *tree){
-  size_t count ;
-  size_t dirleft,dirright;
-  if (tree == NULL){
-    return 0;
-  }
+size_t count ;
+size_t dirleft,dirright;
+if (tree == NULL){
+return 0;
+}
 
-  dirleft = binary_tree_leaves(tree->left);
-  dirright = binary_tree_leaves(tree->right);
-  count = dirleft + dirright;
-  if (tree->left ==NULL && tree->right ==NULL){
-    count = count +1;
-    return count;
-  }
-  return count;
+dirleft = binary_tree_leaves(tree->left);
+dirright = binary_tree_leaves(tree->right);
+count = dirleft + dirright;
+if (tree->left ==NULL && tree->right ==NULL){
+count = count +1;
+return count;
+}
+return count;
 }
 
