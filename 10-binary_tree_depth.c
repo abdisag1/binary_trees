d7d0f47685ac342d01079814ec0c0cfbf73fe25b
abdisag1 <myabdisa@gmail.com>
2022-02-18 18:07:25 +0000

    fixed betty styles

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,14 +8,13 @@
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
-  size_t count = 0;
-
-  if (node == NULL)
-    return (count);
-  while (node->parent != NULL)
-    {
-      count++;
-      node = node->parent;
-    }
-  return (count);
+size_t count = 0;
+if (node == NULL)
+return (count);
+while (node->parent != NULL)
+{
+count++;
+node = node->parent;
+}
+return (count);
 }
